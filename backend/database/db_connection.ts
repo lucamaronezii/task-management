5b05283da5e9c408b002db00ec9abcb7d7ed6f33
Luca Maronezi Steyer <steyerluca@hotmail.com>
2024-09-23 11:36:23 -0300

    .

--- backend/database/db_connection.ts
+++ backend/database/db_connection.ts
@@ -1,11 +1,13 @@
 import knex, { Knex } from 'knex'
-import { env } from '../src/env'
-console.log(process.env.DATABASE_URL)
+import dotnev from 'dotenv'
+
+dotnev.config()
+
 export const dbConfig: Knex.Config = {
     client: 'pg',
     connection: {
         database: process.env.DATABASE_URL,
-        port: env.PORT
+        port: Number(process.env.PORT)
     },
     migrations: {
         extension: 'ts',
